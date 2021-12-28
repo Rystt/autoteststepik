e2cb7f0146bdafc27dbb58d4aa56d4992ccf6575
rf.kagirov <rf.kagirov@tensor.ru>
2021-12-28 18:05:22 +0500

    все уроки

---
+++ lesson2.2.2.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import time
+import math
+from selenium.webdriver.support.ui import Select
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/selects1.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+x_element = browser.find_element_by_id("num1")
+y_element = browser.find_element_by_id("num2")
+x = int(x_element.text) + int(y_element.text)
+y = str(x)
+select = Select(browser.find_element_by_id("dropdown"))
+select.select_by_visible_text(y)
+
+input4 = browser.find_element_by_xpath("//button[text()='Submit']")
+input4.click()
+
+# ожидание чтобы визуально оценить результаты прохождения скрипта
+time.sleep(10)
+# закрываем браузер после всех манипуляций
+browser.quit()
+#x = x_element.get_attribute("valuex")
+#y = calc(x)
+# input1 = browser.find_element_by_id("answer")
+# input1.send_keys(x)
+# input2 = browser.find_element_by_id("robotCheckbox")
+# input2.click()
+# input3 = browser.find_element_by_id("robotsRule")
+# input3.click()
+# input4 = browser.find_element_by_xpath("//button[text()='Submit']")
+# input4.click()
---
+++ lesson2.3.4.py
@@ -0,0 +1,27 @@
+import os
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+browser = webdriver.Chrome()
+link = "http://suninjuly.github.io/alert_accept.html"
+browser.get(link)
+input1 = browser.find_element_by_xpath("//button[text()='I want to go on a magical journey!']")
+input1.click()
+confirm = browser.switch_to.alert
+confirm.accept()
+
+x_element = browser.find_element_by_id("input_value")
+x = x_element.text
+y = calc(x)
+
+input2 = browser.find_element_by_id("answer")
+input2.send_keys(y)
+input4 = browser.find_element_by_xpath("//button[text()='Submit']")
+input4.click()
+
+time.sleep(10)
+browser.quit()
