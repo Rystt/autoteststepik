f007e4aeed514f75bfb93912385499cf71c776ab
Rystt <96776302+Rystt@users.noreply.github.com>
2022-01-07 13:44:45 +0500

    Delete lesson2.4.6.py

--- lesson2.4.6.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-browser = webdriver.Chrome()
-link = "http://suninjuly.github.io/cats.html"
-browser.get(link)
-browser.find_element_by_id("button")
-
-time.sleep(10)
-browser.quit()
