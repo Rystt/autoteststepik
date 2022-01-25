0348e0c3cbdbb6bdb08732d691f82f443db43e5b
rf.kagirov <rf.kagirov@tensor.ru>
2022-01-25 18:34:40 +0500

    добавлены новые локаторы

--- pages/locators.py
+++ pages/locators.py
@@ -1,16 +1,24 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.ID, "registration_link")
+
+# class MainPageLocators():
+#     LOGIN_LINK = (By.ID, "registration_link")
 
 
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
-    # LOGIN_EMAIL = (By.ID, "id_registration-email")
-    # LOGIN_PASS_REG1 = (By.ID, "id_registration-password1")
-    # LOGIN_PASS_REG2 = (By.ID, "id_registration-password2")
+
+class ProductPageLocators():
+    INPUT_ADD_TO_SHOPPING_CART = (By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    INPUT_PRODUCT = (By.XPATH, "//li[@class='active']")
+    MASSEGE_OF_PRODUCT = (By.XPATH, "//div[@id='messages']/div[1]/div/strong")
+    PRICE = (By.XPATH, "//p[@class='price_color']")
+    MASSAGE_OF_PRICE = (By.XPATH, "//div[@id='messages']/div[3]/p/strong")
 
 
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
