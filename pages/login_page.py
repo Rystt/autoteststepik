8d59a8e90c188aeca741f40986698a388865b371
rf.kagirov <rf.kagirov@tensor.ru>
2022-01-25 18:51:12 +0500

    login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,6 +8,7 @@ class LoginPage(BasePage):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
+        self.go_to_login_page()
 
     def should_be_login_url(self):
         assert "login" in self.browser.current_url, "'login' not in current url"
@@ -19,5 +20,7 @@ class LoginPage(BasePage):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Не нашли форму регистрации"
 
     def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link = self.browser.find_element_by_css_selector("#login_link")
         link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
