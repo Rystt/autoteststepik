a858e9f708210a31e3a8da5db578e49b9126a138
rf.kagirov <rf.kagirov@tensor.ru>
2022-01-27 14:42:56 +0500

    добавдены методы проверки отсуствия блока с товаром в пустой корзине и наличия сообщения о том что корзина пуста

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -7,9 +7,15 @@ from selenium.webdriver.support import expected_conditions as EC
 import math
 from .locators import BasePageLocators
 from .locators import BaseCap
+from .login_page import LoginPage
+from .product_page import Product_Page
+from .base_page import BasePage
+import time
+import pytest
 
 
-class BasketPage():
+
+class BasketPage(BasePage):
     def transition_in_cap(self):
         cap = self.browser.find_element(*BaseCap.BASKET_LINK)
         cap.click(), "Не нажалась кнопка перехода в корзину"
@@ -17,6 +23,7 @@ class BasketPage():
     def not_product_in_cap(self):
         assert self.is_not_element_present(*BaseCap.PRODUCT_BLOCK), "Есть блок с товарами в пустой корзине"
 
+#данная проверка так реализваона из за комментария преподавателя, что достаточно проверки на английском
     def text_not_product_empty(self):
         a = (self.browser.find_element(*BaseCap.TEXT_CAP_EMPTY)).text
         assert "Your basket is empty" in a, "Нет текста 'Your basket is empty' в пустой корзине"
