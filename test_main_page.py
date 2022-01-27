6e073008b1abf9608b77478f1588f1527da9ba18
rf.kagirov <rf.kagirov@tensor.ru>
2022-01-27 15:00:30 +0500

    добавлены тесты на переход в корзину и проверки там

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,10 @@
 import time
+import pytest
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.base_page import BasePage
+from pages.basket_page import BasketPage
+
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -42,5 +46,17 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+#@pytest.mark.new
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.transition_in_cap()
+    page.not_product_in_cap()
+    page.text_not_product_empty()
+
+
+
 
 
