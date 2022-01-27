949815bbfadba39118f4a7e6b7503d47316877ff
rf.kagirov <rf.kagirov@tensor.ru>
2022-01-27 15:10:50 +0500

    добавлены тесты на переход в корзину и проверки там

--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,7 @@ from pages.product_page import Product_Page
 from pages.base_page import BasePage
 import time
 import pytest
-
+from pages.basket_page import BasketPage
 
 
 #отрицательные проверки
@@ -63,3 +63,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+#@pytest.mark.new
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.transition_in_cap()
+    page.not_product_in_cap()
+    page.text_not_product_empty()
