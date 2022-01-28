9e201e8e6f23434e96253f7adf86bb3b5542bc70
rf.kagirov <rf.kagirov@tensor.ru>
2022-01-28 16:09:28 +0500

    тесты почищен закомментированный код

--- test_main_page.py
+++ test_main_page.py
@@ -9,17 +9,18 @@ from pages.basket_page import BasketPage
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser,link)
+        page.open()
+        page.go_to_login_page()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 def test_pole_logina_autorization(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -39,6 +40,7 @@ def test_login_v_url(browser):
     page.open()
     page.should_be_login_url()
 
+@pytest.mark.new
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
@@ -47,7 +49,7 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-#@pytest.mark.new
+
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
     page = BasketPage(browser, link)
@@ -55,6 +57,10 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.transition_in_cap()
     page.not_product_in_cap()
     page.text_not_product_empty()
+
+
+
+
 
 
 
