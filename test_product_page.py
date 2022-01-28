2e2b719829232b2f39951e77243dc777d907545c
rf.kagirov <rf.kagirov@tensor.ru>
2022-01-28 16:07:03 +0500

    добавлены тесты под фикстурой setup

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,13 @@
 import math
-from pages.login_page import LoginPage
 from pages.product_page import Product_Page
 from pages.base_page import BasePage
 import time
 import pytest
 from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
 
 
-#отрицательные проверки
+
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
@@ -15,6 +15,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.open()
     page.add_product()
     page.should_not_be_success_message()
+
 
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
@@ -64,7 +65,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-#@pytest.mark.new
+
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
@@ -72,3 +73,40 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.transition_in_cap()
     page.not_product_in_cap()
     page.text_not_product_empty()
+
+
+
+
+
+@pytest.mark.new
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        login_page = LoginPage(browser, "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/")
+        login_page.open()
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time()) + str(time.time())
+        login_page.register_new_user(email=email, password=password)
+        current_url = str(browser.current_url)
+        login_page_with_user = BasePage(browser, current_url)
+        login_page_with_user.should_be_authorized_user()
+
+
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207" #временно
+        page = Product_Page(browser, link)
+        page.open()
+        page.add_product()
+        page.checking_for_message_product()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+        page = Product_Page(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+
+
+
+
